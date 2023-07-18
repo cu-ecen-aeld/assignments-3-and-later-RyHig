6c00389927a28084851be5d04f9c06fb791a6a41
Ryan Higgins <ryanjeffhiggins@gmail.com>
2023-07-17 22:24:00 -0500

    arch= is not ARCH= also, the toolchain i originally downloaded didn't have symlinks... So it was causing my container to fail

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -43,18 +43,21 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
     # TODO: Add your kernel build steps here
     echo "mrproper starting"
+    echo "make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper"
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
     #CROSS_COMPILE=${CROSS_COMPILE} arch=${ARCH} make menuconfig
     echo "defconfig"
+    echo "make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper"
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
     echo "creating image"
-    #make -j16 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} %.dtb || RESULT=$?
+    make -j16 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} Image #|| RESULT=$?
     #if [ RESULT -eq 0 ]; then
     #    echo "Success no need to patch"
     #else
     #    git apply ${KERNEL_PATCH_PATH}/dtc-multiple-definition.patch
+    #    echo "make -j16 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} Image"
+    #    make -j16 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} Image
     #fi
-    make -j16 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
 fi
 
 echo "Adding the Image in outdir"
@@ -70,30 +73,28 @@ then
 fi
 
 # TODO: Create necessary base directories
-if [ ! -d "$OUTDIR/rootfs" ]; then
-    mkdir -p ${OUTDIR}/rootfs
-    cd ${OUTDIR}/rootfs
-    mkdir -p bin dev etc home lib lib64 proc sbin sys tmp 
-    mkdir -p usr/bin usr/lib usr/sbin
-    mkdir -p var/log
-fi
+mkdir -p ${OUTDIR}/rootfs
+cd ${OUTDIR}/rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp 
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
-git clone git://busybox.net/busybox.git
+    git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
     make distclean
-    make defconfig
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
-make arch=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
-make arch=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX=../rootfs install
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX=${OUTDIR}/rootfs install
 
 cd ${OUTDIR}/rootfs
 
@@ -102,26 +103,29 @@ ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+echo $SYSROOT
 cp -a $SYSROOT/lib/ld-linux-aarch64.so.1 lib
 cp -a $SYSROOT/lib64/libm* lib64
-cp -a $SYSROOT/lib64/libc* lib64
+cp -a $SYSROOT/lib64/ld-2.31.so lib64
+cp -a $SYSROOT/lib64/libc.so.6 lib64
+cp -a $SYSROOT/lib64/libc-2.31.so lib64
 cp -a $SYSROOT/lib64/libresolv* lib64
 # TODO: Make device nodes
 sudo mknod -m 666 dev/null c 1 3
 sudo mknod -m 600 dev/console c 5 1
 # TODO: Clean and build the writer utility
 cd ${FINDER_APP_DIR}
-make clean || echo "Attempting to clean..."
+echo "trying to clean" || make clean
 make CROSS_COMPILE=${CROSS_COMPILE}
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+echo "attempting to copy finder_app files"
 cp -r ../conf ${OUTDIR}/rootfs/
-cp -a conf writer finder* autorun-qemu.sh ${OUTDIR}/rootfs/home
+cp -a finder* writer autorun-qemu.sh conf ${OUTDIR}/rootfs/home
 # TODO: Chown the root directory
 sudo chown root:root ${OUTDIR}/rootfs
 # TODO: Create initramfs.cpio.gz
-cd "$OUTDIR/rootfs"
+cd ${OUTDIR}/rootfs
 find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
 cd ${OUTDIR}
 gzip -f initramfs.cpio
-mkimage -A ${ARCH} -O linux -T ramdisk -d initramfs.cpio.gz uRamdisk
